9a80d6ec5756fdba97df68ae8e3012f3b092e265
Takeru Yamakawa <21017@daitron.co.jp>
2025-06-21 04:16:08 +0900

    DynamicVoteコントラクトに変更し、デプロイ時のログを更新。DYNAMIC_VOTE_ADDRESSを新しいアドレスに変更。

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,10 @@
 const hre = require('hardhat');
 
 async function main() {
-    const Vote = await hre.ethers.getContractFactory('SimpleVote');
+    const Vote = await hre.ethers.getContractFactory('DynamicVote');
     const vote = await Vote.deploy('Cats vs Dogs');
     await vote.waitForDeployment();
-    console.log('SimpleVote deployed to:', vote.target);
+    console.log('DynamicVote deployed to:', vote.target);
 }
 
 main().catch((error) => {
