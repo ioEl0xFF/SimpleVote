1762e6ce37a7616994c7dc2b5e491f917b5ef262
ioEl <34502134+ioEl0xFF@users.noreply.github.com>
2025-06-21 09:28:21 +0900

    Add default choices on deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,8 @@ async function main() {
     const Vote = await hre.ethers.getContractFactory('DynamicVote');
     const vote = await Vote.deploy('Cats vs Dogs');
     await vote.waitForDeployment();
+    await vote.addChoice('Cats');
+    await vote.addChoice('Dogs');
     console.log('DynamicVote deployed to:', vote.target);
 }
 
